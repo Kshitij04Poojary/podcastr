5f1e11cb6164f4e760353d7eea502692ed23497c
Kshitij04Poojary <kpoojary2004@gmail.com>
2024-07-09 12:46:04 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig= {
+    providers: [
+      {
+        domain: "https://choice-cobra-29.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
